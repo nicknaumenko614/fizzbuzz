78c315cf333154ee86428e17b0c0083b3da0d966
Mel McGee <mel@wecancodeit.org>
2020-05-14 13:12:57 -0400

    Initial commit

---
+++ src/main/java/org/wecancodeit/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,5 @@
+package org.wecancodeit.fizzbuzz;
+
+
+public class FizzBuzz {
+}
---
+++ src/test/java/org/wecancodeit/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,10 @@
+package org.wecancodeit.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+    @Test
+    public void shouldInstantiate() {
+        FizzBuzz underTest = new FizzBuzz();
+    }
+}
